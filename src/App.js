6612f013c845d227a978b482981161425b9c6903
Arnaldo <arnaldo.e.diaz@gmail.com>
2024-06-27 12:46:55 -0400

    Removed boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import "./App.css";
+import './App.css';
 
 function App() {
-  function handleClick() {
-    let randomNum = Math.floor(Math.random() * 3) + 1;
-    console.log(randomNum);
-    let userInput = prompt("type a number");
-    alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
-  }
   return (
     <div className='App'>
-      <h1>Task: Add a button and handle a click event</h1>
-      <button onClick={handleClick}>Guess the number between 1 and 3</button>
+      <h1>Homepage</h1>
     </div>
   );
 }
